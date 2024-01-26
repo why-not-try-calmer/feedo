a29dee3042f71f6c3806cec3c96df6f77db2ef56
why-not-try-calmer <mrnycticorax@gmail.com>
2024-01-26 12:44:26 +0100

    ---

--- scripts/mongo-startup.js
+++ scripts/mongo-startup.js
@@ -2,6 +2,6 @@ db_name = process.env["TEST"] == "1" ? "feedfarer-test" : "feedfarer"
 username = process.env["MONGO_INITDB_ROOT_USERNAME"]
 password = process.env["MONGO_INITDB_ROOT_PASSWORD"]
 conn_str = `mongodb://${username}:${password}@localhost:27017/admin`
-conn = Mongo(conn_str);
+conn = Mongo(conn_str)
 db = conn.getDB(db_name)
 db.items.createIndex({ "i_desc": "text" })
