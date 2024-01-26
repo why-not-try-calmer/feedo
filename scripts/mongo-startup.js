448e3c1a395e22da5609cca5a2ab9ffeff415f75
why-not-try-calmer <mrnycticorax@gmail.com>
2024-01-26 14:16:02 +0100

    ---

--- scripts/mongo-startup.js
+++ scripts/mongo-startup.js
@@ -1,10 +1,13 @@
-db_name = process.env["TEST"] == "1" ? "feedfarer-test" : "feedfarer"
-username = process.env["MONGO_INITDB_ROOT_USERNAME"]
-password = process.env["MONGO_INITDB_ROOT_PASSWORD"]
+const db_name = process.env["TEST"] == "1" ? "feedfarer-test" : "feedfarer"
+const username = process.env["MONGO_INITDB_ROOT_USERNAME"]
+const password = process.env["MONGO_INITDB_ROOT_PASSWORD"]
 new Array(db_name, username, password).forEach(c => console.assert(c))
 
-conn_str = `mongodb://${username}:${password}@localhost:27017/admin`
-conn = Mongo(conn_str)
-db = conn.getDB(db_name)
+const conn_str = `mongodb://${username}:${password}@localhost:27017/admin`
+const conn = new Mongo(conn_str)
+const db = conn.getDB(db_name)
 db.items.createIndex({ "i_desc": "text" })
-console.assert(db.items.getIndexes())
+
+const indexes = db.items.getIndexes()
+const found = indexes.find(idx => idx.name == "i_desc_text")
+console.assert(found)
