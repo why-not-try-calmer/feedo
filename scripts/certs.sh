4c4815a635c519503002c2d91595886f02df566e
why-not-try-calmer <adrien.glauser@gmail.com>
2024-08-06 00:07:47 +0200

    webroot validation for TLS certificates

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-source .env
-export CERT_FULLCHAIN
-export TELEGRAM_TOKEN
+# first run
 
-docker compose stop nginx
+# source .env
+# export CERT_FULLCHAIN
+# export TELEGRAM_TOKEN
+
+# docker run -it --rm --name certbot  \
+#     -v "/certs/etc:/etc/letsencrypt" \
+#     -v "/certs/lib:/var/lib/letsencrypt" \
+#     -v "./certbot/www:/var/www/certbot/" \
+#     certbot/certbot certonly --webroot-path /var/www/certbot -d feedo.cloudns.ph
+
+# curl -F "url=https://feedo.cloudns.ph/webhook/bot$TELEGRAM_TOKEN" https://api.telegram.org/bot$TELEGRAM_TOKEN/setWebhook -F "certificate=${CERT_FULLCHAIN}"
 
 docker run -it --rm --name certbot  \
     -v "/certs/etc:/etc/letsencrypt" \
     -v "/certs/lib:/var/lib/letsencrypt" \
-	-e CERT_FULLCHAIN \
-	-e TELEGRAM_TOKEN \
-	-p 80:80 \
-	certbot/certbot renew > certs_refresh.log
+    -v "./certbot/www:/var/www/certbot/" \
+    certbot/certbot renew
 
-if grep -q "No renewals were attempted." certs_refresh.log; then
-	echo "No need to renew certificate."
-else
-	./scripts/set_webhook.sh
-fi
-
-docker compose start nginx
+docker compose exec nginx service nginx reload -s
--- scripts/set_webhook.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash            
-curl -F "url=https://feedo.cloudns.ph/webhook/bot$TELEGRAM_TOKEN" https://api.telegram.org/bot$TELEGRAM_TOKEN/setWebhook -F "certificate=${CERT_FULLCHAIN}"
