0f57e805652e708abd6a221752ed62da770731d7
why-not-try-calmer <mrnycticorax@gmail.com>
2024-10-12 13:08:51 +0200

    adjusted certs.sh

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -6,7 +6,12 @@
 # export CERT_FULLCHAIN
 # export TELEGRAM_TOKEN
 
-# docker run -it --rm --name certbot -v "/certs/etc:/etc/letsencrypt" -v "/certs/lib:/var/lib/letsencrypt" -v "./certbot/www:/var/www/certbot/" certbot/certbot certonly --webroot -w /var/www/certbot -d feedo.cloudns.ph
+docker run -it --rm --name certbot \
+    -v "/certs/etc:/etc/letsencrypt" \
+    -v "/certs/lib:/var/lib/letsencrypt" \
+    -v "./certbot/www:/var/www/certbot/" \
+    certbot/certbot certonly --webroot -w /var/www/certbot -d feedo.cloudns.ph
+
 # curl -F "url=https://feedo.cloudns.ph/webhook/bot$TELEGRAM_TOKEN" https://api.telegram.org/bot$TELEGRAM_TOKEN/setWebhook -F "certificate=${CERT_FULLCHAIN}"
 
 docker run -t --rm --name certbot  \
@@ -15,4 +20,4 @@ docker run -t --rm --name certbot  \
     -v "./certbot/www:/var/www/certbot/" \
     certbot/certbot renew -n
 
-docker compose exec nginx service nginx reload -s
+docker compose restart nginx
